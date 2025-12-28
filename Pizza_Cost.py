17ad3d30aa367bea3422222ff7a1479ff4267f6e
PrabhuGiridhari <cvpgiridhari@gmail.com>
2025-12-28 14:31:22 +0530

    new line added

--- Pizza_Cost.py
+++ Pizza_Cost.py
@@ -1,5 +1,24 @@
+# Price details as below
+# Small pizza (S): ₹15
+# Medium pizza (M): ₹20
+# Large pizza (L): ₹25
+# Add pepperoni for small pizza (Y or N): +₹2
+# Add pepperoni for medium or large pizza (Y or N): +₹3
+# Add extra cheese for any size pizza (Y or N): +₹1
+
 print ("Welcome to Prabhu's Pizza Corner")
 print ("Tell us how we can make your pizza perfect!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+price = 0
+
+if size == "S" or size == "s":
+  price = 15
+  elif size == "M" or size == "m":
+    price = 20
+  elif size == "L" or size == "l":
+    price = 25
+  if pepperoni == "Y" or pepperoni == "y":
+    price += 2
+elif pepperoni == "Y" or pepperoni == "y":
