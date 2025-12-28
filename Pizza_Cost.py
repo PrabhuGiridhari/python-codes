b67ce53b3942576c389d004548f6d98fab89e547
PrabhuGiridhari <cvpgiridhari@gmail.com>
2025-12-28 16:52:15 +0530

    final code updated

--- Pizza_Cost.py
+++ Pizza_Cost.py
@@ -26,6 +26,8 @@ else:
         price += 20
     elif size == 'L' or size == 'l':
         price += 25
+    elif size != 'S' and size != 's' and size != 'M' and size != 'm' and size != 'L' and size != 'l':
+        print("Invalid size selected.")
     if pepperoni == "Y" or pepperoni == "y":
         price += 3
     if extra_cheese == "Y" or extra_cheese == "y":
