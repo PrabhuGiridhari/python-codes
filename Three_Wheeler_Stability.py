d1f091108806c9a953c0e0c653d884d8d5e1fbd7
PrabhuGiridhari <cvpgiridhari@gmail.com>
2026-01-15 21:02:12 +0530

    Speed Formula Added

--- Three_Wheeler_Stability.py
+++ Three_Wheeler_Stability.py
@@ -1,5 +1,6 @@
 # import time
 # import random
+import math
 
 # input requirements to calculate stability
 try:
@@ -7,9 +8,9 @@ try:
     wheel_track = int(input("Wheel Track in mm: ")) / 1000 #Value in mm > converted to meters
     wheel_base = int(input("Wheel Base in mm: ")) / 1000 #Value in mm > converted to meters
     cg_height = int(input("CoG from Ground (Z Dir) in mm: ")) / 1000 #Value in mm > converted to meters
-    TCD = int(input("TCD in m: "))
+    tcd = int(input("TCD in m: "))
     gravity = 9.81 #m/sec2
-    print (wheel_base, wheel_track, cg_height)
+    # print (wheel_base, wheel_track, cg_height)
 except ValueError as e:
     print("Enter a numeric value :(")
 except ZeroDivisionError as k:
@@ -21,8 +22,15 @@ def static_rollover_stability(a,b):
     return round(value,3)
 ssf = static_rollover_stability(wheel_track, cg_height)
 if ssf >= 1:
-    print(f"Your Static Rollover Stability value is {ssf}, Excellent. \nNote: Any value greater or equal to 1 is excellent")
+    print(f"Your Static Rollover Stability value is {ssf}g, Excellent. \nNote: Any value greater or equal to 1 is excellent")
 elif 0.8 <=ssf <= 1:
-    print(f"Your Static Rollover Stability value is {ssf}, Good. \nNote: Any value in between 0.8 ~ 0.99 is acceptable")
+    print(f"Your Static Rollover Stability value is {ssf}g, Good. \nNote: Any value in between 0.8 ~ 0.99 is acceptable")
 else:
-    print(f"Your Static Rollover Stability value is {ssf}, Not Good. \nNote: Any value below 0.8 is at risk of rolling")
+    print(f"Your Static Rollover Stability value is {ssf}g, Not Good. \nNote: Any value below 0.8 is at risk of rolling")
+
+# Calculating Critical Lateral Acceleration & it is also known as Static Roll Over Stability
+critical_lateral_acceleration = ssf * gravity #output value in m/sec2
+
+# Calculating Safe turning speed for your vehicle geometry
+safe_turning_speed = math.sqrt(critical_lateral_acceleration * tcd) * 3.6 # Multiply by 3.6 to convert m/sec2 to Km/h
+print (round(safe_turning_speed,1),"km/h")
