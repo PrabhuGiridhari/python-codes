b41bbf8fc7c1360d68b85233ae21ce543d9057df
PrabhuGiridhari <cvpgiridhari@gmail.com>
2026-01-15 16:32:52 +0530

    Code updated

--- Three_Wheeler_Stability.py
+++ Three_Wheeler_Stability.py
@@ -7,9 +7,15 @@ try:
     wheel_track = int(input("Wheel Track in mm: ")) / 1000 #Value converted to meters
     wheel_base = int(input("Wheel Base in mm: ")) / 1000 #Value converted to meters
     CG_Height = int(input("CoG from Ground (Z Dir) in mm: ")) / 1000 #Value converted to meters
+    TCD = int(input("TCD in m: "))
     gravity = 9.81 #m/sec2
     print (wheel_base, wheel_track, CG_Height)
 except ValueError as e:
     print("Enter a numeric value :(")
 except ZeroDivisionError as e:
     print("Zero cannot be taken into account :(")
+
+# Calculating Static Roll Over Stability
+def Static_Rollover_Stability(a,b):
+    SSF = a / 2b
+    return print (f"Your Static Rollover Stability At Standstill / Low Speed is {SSF} ")
