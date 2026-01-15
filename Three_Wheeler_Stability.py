ab88a8bb86c894a5b31e3bf458fde961bb5da938
PrabhuGiridhari <cvpgiridhari@gmail.com>
2026-01-15 17:59:47 +0530

    update

--- Three_Wheeler_Stability.py
+++ Three_Wheeler_Stability.py
@@ -4,9 +4,9 @@
 # input requirements to calculate stability
 try:
     print ("Provide below values to calculate stability of given 3 wheeled vehicle")
-    wheel_track = int(input("Wheel Track in mm: ")) / 1000 #Value converted to meters
-    wheel_base = int(input("Wheel Base in mm: ")) / 1000 #Value converted to meters
-    cg_height = int(input("CoG from Ground (Z Dir) in mm: ")) / 1000 #Value converted to meters
+    wheel_track = int(input("Wheel Track in mm: ")) / 1000 #Value in mm > converted to meters
+    wheel_base = int(input("Wheel Base in mm: ")) / 1000 #Value in mm > converted to meters
+    cg_height = int(input("CoG from Ground (Z Dir) in mm: ")) / 1000 #Value in mm > converted to meters
     TCD = int(input("TCD in m: "))
     gravity = 9.81 #m/sec2
     print (wheel_base, wheel_track, cg_height)
